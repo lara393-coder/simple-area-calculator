60c4a4cbfaf55bc5948c23b1c7486f7cc412dc8a
lara393-coder <laraibrahem34@gmail.com>
2025-06-21 13:49:35 +0300

    Add files via upload

---
+++ area-calculator.py
@@ -0,0 +1,27 @@
+str_length=input("please type length: \n")
+str_width=input("please type width: \n")
+str_price=input("how much for 1 meter? \n")
+
+length=float(str_length)
+width=float(str_width)
+price=float(str_price)
+
+area=length*width
+total_price=price * area
+
+str_area=str(area)
+str_total_price=str(total_price)
+
+print("the total area is:" + str_area )
+print("give the gay:$" + str_total_price)
+
+
+length=float(input("length:\n"))
+weidth=float(input("wiedth:\n"))
+price=float(input("how nuch for 1 meter:\n"))
+erea=length* weidth
+worker=erea*price
+Erea=str(erea)
+str_price=str(worker)
+print("the total erea is:" + Erea)
+print("Give the guy:"+str_price)
